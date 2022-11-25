428692156a44cca6d87b46ceb0a0af0f6be75fcd
Andrea Biondaro <andrea.biondaro@addvalue.it>
2022-11-25 15:44:21 +0100

    added gitpod.yml

--- batch/spotify/pandas_reader.py
+++ batch/spotify/pandas_reader.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import time
 
 
 def main():
+    start = time.time()
     df = pd.read_csv('../../resources/spotify/dataset.csv', encoding='utf8', skiprows=[1])
     pd.set_option('display.max_rows', None)
     print(df)
+    end = time.time()
+    print(f'Total time taken to run: {(end - start) * 1000}ms')
 
 
 if __name__ == '__main__':
--- helloworld/hello_world.py
+++ helloworld/hello_world.py
@@ -1,6 +1,12 @@
+import time
+
 
 def main():
+    start = time.time()
     print('Hello World')
+    end = time.time()
+
+    print(f'Total time taken to run: {(end - start) * 1000}ms')
 
 
 if __name__ == '__main__':
